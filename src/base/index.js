7b54ba366002033f3c5d068955ef7965ec3cc3ae
Gospel Chinyereugo <gospelokpara@gmail.com>
2023-08-27 04:56:33 +0100

    update api base url

--- src/base/index.js
+++ src/base/index.js
@@ -1,7 +1,7 @@
 export const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:9009"
-    : "https://awasource.herokuapp.com";
+    : "https://awasource-api.onrender.com";
 
 export const adminRoute = `api/admin`;
 
